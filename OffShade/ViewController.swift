de3ab0635909a54350f70c57ea8f17eae67ca73c
Timothy Yang <timothyy208@gmail.com>
2019-07-29 00:40:37 -0700

    start game button, logo

--- OffShade/ViewController.swift
+++ OffShade/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startGameButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startGameButton.backgroundColor = .clear
+        startGameButton.layer.cornerRadius = 5
+        startGameButton.layer.borderWidth = 1
+        startGameButton.layer.borderColor = UIColor.black.cgColor
     }
 
 
