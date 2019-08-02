19931fc6c7be06b5df135b1be05864388126aac6
Timothy Yang <timothyy208@gmail.com>
2019-08-02 08:24:41 -0700

    added timer

--- OffShade/GameWindow.swift
+++ OffShade/GameWindow.swift
@@ -10,21 +10,58 @@ import UIKit
 
 class GameWindow: UIViewController {
 
+    
+    var countdownTimer: Timer!
+    var totalTime = 60
+    var timerLabel: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let screenSize: CGRect = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        timerLabel = UITextView(frame: CGRect(x: (screenWidth-50)/2, y: 100, width: 100, height: 100))
+        timerLabel.font = UIFont.systemFont(ofSize: 25)
+        self.view.addSubview(timerLabel)
+        generateButtons()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func buttonAction() {
+        startTimer()
     }
-    */
+ 
+    func generateButtons() {
+        let button = UIButton(frame: CGRect(x: 100, y: 200, width: 100, height: 50))
 
+        button.setTitle("Test Button", for: [])
+        button.backgroundColor = .clear
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+    }
+    
+    func startTimer() {
+        countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTime() {
+        timerLabel.text = "\(totalTime)"
+        
+        if totalTime != 0 {
+            totalTime -= 1
+        } else {
+            endTimer()
+        }
+    }
+    
+    func endTimer() {
+        countdownTimer.invalidate()
+        print("you lost")
+    }
 }
